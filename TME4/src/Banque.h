7e1158509a5fba09e6148bf2ccbe92d0eea7a370
FX-Drouard <fxdrouard@gmail.com>
2023-10-20 15:59:52 +0200

    TME5 pas fini

--- TME4/src/Banque.h
+++ TME4/src/Banque.h
@@ -2,15 +2,24 @@
 
 #include "Compte.h"
 #include <vector>
+#include <iostream>
 
 namespace pr {
 
 class Banque {
 	typedef std::vector<Compte> comptes_t;
 	comptes_t comptes;
+	//mutable std::mutex *mtx;
 public :
-	Banque (size_t ncomptes, size_t solde) : comptes (ncomptes, Compte(solde)){
+	Banque (size_t ncomptes, size_t solde) : comptes(ncomptes, Compte(solde)){
+		//mtx = new std::mutex();
 	}
+//	Banque (const Banque & other) {
+//		std::cout << "copy called" << std::endl;
+//		other.mtx.lock();
+////		this->comptes = other.comptes;
+//		other.mtx.unlock();
+//	}
 	void transfert(size_t deb, size_t cred, unsigned int val) ;
 	size_t size() const ;
 	bool comptabiliser (int attendu) const ;
--- TME4/src/main.cpp
+++ TME4/src/main.cpp
@@ -1,13 +1,48 @@
 #include "Banque.h"
-
+#include "unistd.h"
+#include <iostream>
 using namespace std;
+using namespace pr;
 
 const int NB_THREAD = 10;
+
+void work (Banque & b){
+	size_t i = std::rand() % b.size();
+	size_t j = std::rand() % b.size();
+	unsigned int m = std::rand() % 100+1;
+	b.transfert(i,j,m);
+	size_t t= std::rand() % 20;
+	std::this_thread::sleep_for(std::chrono::milliseconds(t));
+
+}
+
+void shift(int id,Banque & b){
+	std::cout<<"Start Shift "<<id<<std::endl;
+	for(int i=0;i<1000;i++){
+		work(b);
+	}
+	std::cout<<"End Shift "<<id<<std::endl;
+}
+
+void comptable(Banque & b){
+	std::cout<<"\tStart Shift COMPTABLE "<<std::endl;
+	for (int i=0;i<10;i++){
+		b.comptabiliser(2000*100);
+	}
+	std::cout<<"\tEND Shift COMPTABLE "<<std::endl;
+}
 int main () {
+	Banque bank= Banque(2000, 100);
+	std::cout << "banqye created" << std::endl;
 	vector<thread> threads;
 	// TODO : creer des threads qui font ce qui est demandé
+	std::cout<<"I START"<<std::endl;
 
-
+	for (int i=0; i<NB_THREAD; ++i){
+		std::cout<<"CREATE "<<i<<std::endl;
+		threads.emplace_back(shift, i, std::ref(bank));
+	}
+	threads.emplace_back(comptable,std::ref(bank));
 	for (auto & t : threads) {
 		t.join();
 	}
